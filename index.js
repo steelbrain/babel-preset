565db2ee8ade70df944c3af0349a4a7262c85836
steelbrain <de.steelbrain@gmail.com>
2016-03-06 05:48:07 -0700

    :art: Merge babel-preset-steelbrain-async in

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ module.exports = {
     require("babel-plugin-transform-es2015-modules-commonjs"),
     require("babel-plugin-transform-es2015-for-of"),
     require("babel-plugin-transform-es2015-arrow-functions"),
-    require("babel-plugin-transform-es2015-spread")
+    require("babel-plugin-transform-es2015-spread"),
+    require("babel-plugin-syntax-async-functions"),
+    require("babel-plugin-syntax-async-generators"),
+    require("babel-plugin-transform-async-to-generator")
   ]
 }
